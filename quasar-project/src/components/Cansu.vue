ed477f325592100f7767ffa7d5f0fe57381a5bf3
Cansu KAHRAMAN <canssukahraman@gmail.com>
2024-01-20 23:39:47 +0300

    initial fix firabase entegration

---
+++ quasar-project/src/components/Cansu.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>merhaba cansu</p>
+</template>
+
+<script lang="ts">
+
+import { defineComponent} from 'vue'
+
+export default defineComponent({
+    name:'Cansu',
+    setup() {
+        return('Merhaba');
+    },
+})
+</script>
+
+
